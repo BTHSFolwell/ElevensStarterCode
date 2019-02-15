bdecd2fbf4d0879f3c6e94c29ae66d4351882fa6
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2019-02-15 08:34:16 -0500

    Update CardTester.java

--- src/Activity1/CardTester.java
+++ src/Activity1/CardTester.java
@@ -4,4 +4,12 @@ package Activity1;
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+  public static void main(String[] args){
+        Card card1= new Card("A","Spades", 1);
+        Card card2= new Card("3","Hearts", 3);
+        Card card3= new Card("6","Diamonds", 9);
+
+        System.out.println(card1.matches(card2));
+        System.out.println(card2.matches(card3));
+    }
 }
